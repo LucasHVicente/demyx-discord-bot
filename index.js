3ea711bbafe309f33726340ab925f20a6dac1655
Vince <l.vicente9003@gmail.com>
2020-10-06 11:08:57 -0300

    !skip e fila de reprodução implementados

--- index.js
+++ index.js
@@ -23,6 +23,9 @@ const {
 	token,
 } = require('./config.json');
 
+const queue = new Map()
+
+
 bot.login(token);
 
 bot.once('ready',() =>{
@@ -30,7 +33,7 @@ bot.once('ready',() =>{
 });
 
 bot.on('guildMemberAdd', membro=>{
-  const quote = demyxQuotes.musicPlayQuotes[Math.floor(Math.random() * demyxQuotes.welcomeQuotes.length)]
+  const quote = demyxQuotes.welcomeQuotes[Math.floor(Math.random() * demyxQuotes.welcomeQuotes.length)]
   membro.send(quote);
 })
 
@@ -42,32 +45,95 @@ bot.on('message',async msg=>{//comandos de mensagens
   const voiceChannel = msg.member.voice.channel;
   const args = msg.content.substring(prefix.length).split(" ");
 
+  const serverQueue = queue.get(msg.guild.id)
+
   if(msg.content.startsWith(`${prefix}play`)){
-    if (msg.member.voice.channel) {
-      const connection = await voiceChannel.join();
-      const quote = demyxQuotes.musicPlayQuotes[Math.floor(Math.random() * demyxQuotes.musicPlayQuotes.length)]
-      msg.channel.send(quote + args[1]);
-      const dispatcher = connection.play(ytdl(args[1]))
-      .on('finish', ()=>voiceChannel.leave())
-      .on('error', err=>{
-        console.log(err);
-      })
-      dispatcher.setVolumeLogarithmic(5 / 5);
-    } else {
-      msg.reply('Voce precisa entrar em um canal de voz antes!');
+
+    if(!voiceChannel) return msg.channel.send('You neet to be in a voice channel first');
+    const permissions = voiceChannel.permissionsFor(msg.client.user);
+    if(!permissions.has('CONNECT')) return msg.channel.send("Hey! I don't have permission to connect to the voice channel!")
+    if(!permissions.has('SPEAK')) return msg.channel.send("Hey! I don't have permission to speak in the channel!")
+
+    const songInfo = await ytdl.getInfo(args[1])
+    const song = {
+      title: songInfo.videoDetails.title,
+      url: songInfo.videoDetails.video_url
     }
+
+    if(!serverQueue){
+      const queueConstuct = {
+        textChannel: msg.channel,
+        voiceChannel: voiceChannel,
+        connection: null,
+        songs: [],
+        volume: 5,
+        playing: true
+      }
+      queue.set(msg.guild.id, queueConstuct)
+
+      queueConstuct.songs.push(song);
+      try {
+        const connection = await voiceChannel.join();
+        const quote = demyxQuotes.musicPlayQuotes[Math.floor(Math.random() * demyxQuotes.musicPlayQuotes.length)]
+        msg.channel.send(quote+ ' ' + queueConstuct.songs[0].title);
+        queueConstuct.connection = connection;
+        
+        play(msg.guild, queueConstuct.songs[0]);
+        
+        
+      } catch (error) {
+        console.log(error);
+        queue.delete(msg.guild.id)
+        return msg.channel.send('There was an error while joining the voice channel:', error);
+      }
+    }else{
+      serverQueue.songs.push(song)
+      const quote = demyxQuotes.musicPlayQuotes[Math.floor(Math.random() * demyxQuotes.musicPlayQuotes.length)]
+        
+      return msg.channel.send(`${quote} ${song.title} has been added to the queue!`)
+    }
+    return undefined;
+
   }else {
     if(msg.content.startsWith(`${prefix}stop`)){
-      if(!voiceChannel) return msg.channel.send('Voce precisa entrar em um canal de voz antes')
+      if(!voiceChannel) return msg.channel.send('You need to be in a voice channel first')
+      if(!serverQueue) return msg.channel.send('There is nothing playing')
+
+      serverQueue.songs = []
+      serverQueue.connection.dispatcher.end()
+
+      const quote = demyxQuotes.musicStopQuotes[Math.floor(Math.random() * demyxQuotes.musicStopQuotes.length)]
+      msg.channel.send(quote);
+
       voiceChannel.leave();
-      const quote = demyxQuotes.musicStopQuotes[Math.floor(Math.random() * demyxQuotes.musicPlayQuotes.length)]
-      return msg.channel.send(quote);
+
+    }else if(msg.content.startsWith(`${prefix}skip`)){
+      if(!voiceChannel) return msg.channel.send('You need to be in a voice channel first')
+      if(!serverQueue) return msg.channel.send('There is nothing playing')
+
+      serverQueue.connection.dispatcher.end()
+      msg.channel.send("Okay then, let's skip to the next one.");
+      return undefined;
     }
   }
-  if(msg.content === prefix+'dance'){//comando de dance
-    const msgAttachment = new Discord.MessageAttachment('https://i.imgur.com/w3duR07.png');
-    msg.reply('Dance water! Dance!', msgAttachment);
-
-  }
-  
 })
+
+function play(guild, song){
+  const serverQueue = queue.get(guild.id);
+
+  if(!song){
+    serverQueue.voiceChannel.leave();
+    queue.delete(guild.id);
+    return
+  }
+
+  const dispatcher = serverQueue.connection.play(ytdl(song.url))
+    .on('finish', ()=>{
+      serverQueue.songs.shift()
+      play(guild, serverQueue.songs[0])
+    })
+    .on('error', err=>{
+      console.log(err);
+    })
+    dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+}
